bd5a0e8500982dcbc1ac37e91bf848a7bbf34a7f
Evgenii Stetsenko <i@estetsenko.com>
2025-12-23 18:02:53 +0300

    fix: reimplement sync barrier to avoid race + ordering bug

--- maas-kafka-client/src/test/java/com/netcracker/maas/declarative/kafka/client/SyncBarrier.java
+++ maas-kafka-client/src/test/java/com/netcracker/maas/declarative/kafka/client/SyncBarrier.java
@@ -1,33 +1,32 @@
 package com.netcracker.maas.declarative.kafka.client;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.Duration;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 @Slf4j
 public class SyncBarrier {
-    private final ConcurrentMap<String, CountDownLatch> latches = new ConcurrentHashMap<>();
+
+    private final ConcurrentMap<String, Semaphore> events = new ConcurrentHashMap<>();
 
     public void notify(String eventName) {
         log.info("Notify: {}", eventName);
-        latches.computeIfAbsent(eventName, k -> new CountDownLatch(1)).countDown();
+        events.computeIfAbsent(eventName, k -> new Semaphore(0)).release();
     }
 
+    @SneakyThrows
     public void await(String eventName, Duration timeout) {
         log.info("Await: {} (timeout: {})", eventName, timeout);
-        var latch = latches.computeIfAbsent(eventName, k -> new CountDownLatch(1));
-        try {
-            if (!latch.await(timeout.toMillis(), TimeUnit.MILLISECONDS)) {
-                fail("timeout waiting event: " + eventName);
-            }
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        var sem = events.computeIfAbsent(eventName, k -> new Semaphore(0));
+        if (!sem.tryAcquire(timeout.toMillis(), TimeUnit.MILLISECONDS)) {
+            fail("timeout waiting event: " + eventName);
         }
         log.info("Event received: {}", eventName);
     }
